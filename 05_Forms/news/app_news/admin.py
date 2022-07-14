e2fb065bcbf0abd70d12e05fe8f927eae6d3aa4c
trunova <lion-n-t@mail.ru>
2022-07-14 20:02:05 +0300

    Практическая работа 7 модуля

--- 05_Forms/news/app_news/admin.py
+++ 05_Forms/news/app_news/admin.py
@@ -1,10 +1,35 @@
 from django.contrib import admin
 from app_news.models import News, Comment
 
+
+class CommentInLine(admin.TabularInline):
+    model = Comment
+
+
 @admin.register(News)
 class NewsAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['title', 'create_date', 'edit_date', 'flag']
+    list_filter = ['flag']
+    inlines = [CommentInLine]
+    actions = ['mark_as_actively', 'mark_as_not_active']
+
+    def mark_as_not_active(self, request, queryset):
+        queryset.update(flag=False)
+
+    def mark_as_actively(self, request, queryset):
+        queryset.update(flag=True)
+
+    mark_as_actively.short_description = 'Перевести в статус \"активно\"'
+    mark_as_not_active.short_description = 'Перевести в статус \"неактивно\"'
+
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['username', 'short_comment_text', 'news']
+    list_filter = ['username']
+    actions = ['removed_by_admin']
+
+    def removed_by_admin(self, request, queryset):
+        queryset.update(comment_text='Удалено администратором')
+
+    removed_by_admin.short_description = 'Проставить текст комментария \"Удалено администратором\"'
--- 05_Forms/news/app_news/models.py
+++ 05_Forms/news/app_news/models.py
@@ -1,17 +1,29 @@
 from django.db import models
+from django.template.defaultfilters import truncatechars  # or truncatewords
 
 class News(models.Model):
+    FLAG_CHOICES = [
+        (True, 'actively'),
+        (False, 'not active')
+    ]
     title = models.CharField(verbose_name='Название', max_length=100)
     content = models.TextField(verbose_name='Текст')
     create_date = models.DateTimeField(auto_now_add=True)
     edit_date = models.DateTimeField(auto_now=True)
-    flag = models.BooleanField()
+    flag = models.BooleanField(primary_key=False, choices=FLAG_CHOICES, default='n')
 
     def __str__(self):
-        return self.title
+        return f'{self.title}, {self.create_date}, {self.flag}'
 
 class Comment(models.Model):
     username = models.CharField(verbose_name='Username', max_length=100)
     comment_text = models.TextField(verbose_name='Комментарий')
     news = models.ForeignKey('News', default=None, null=True,
                                on_delete=models.CASCADE, related_name='ad_news')
+
+    @property
+    def short_comment_text(self):
+        return truncatechars(self.comment_text, 15)
+
+    def __str__(self):
+        return f'{self.username}, {self.short_comment_text}'
